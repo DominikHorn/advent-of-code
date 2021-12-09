cd1e0e9728639921e11d0bdd074f8726ae948742
Dominik Horn <dominik.horn@tum.de>
2021-12-09 14:58:06 +0100

    [08] fix description missing spaces

--- 2021/Sources/08/main.swift
+++ 2021/Sources/08/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/**
+/*
  --- Day 8: Seven Segment Search ---
  
  You barely reach the safety of the cave when the whale smashes into the cave mouth, collapsing it. Sensors indicate another exit to this cave at a much greater depth, so you have no choice but to press on.
@@ -10,22 +10,22 @@ import Foundation
  Each digit of a seven-segment display is rendered by turning on or off any of seven segments named a through g:
  
  0:      1:      2:      3:      4:
- aaaa    ....    aaaa    aaaa    ....
+  aaaa    ....    aaaa    aaaa    ....
  b    c  .    c  .    c  .    c  b    c
  b    c  .    c  .    c  .    c  b    c
- ....    ....    dddd    dddd    dddd
+  ....    ....    dddd    dddd    dddd
  e    f  .    f  e    .  .    f  .    f
  e    f  .    f  e    .  .    f  .    f
- gggg    ....    gggg    gggg    ....
+  gggg    ....    gggg    gggg    ....
  
  5:      6:      7:      8:      9:
- aaaa    aaaa    aaaa    aaaa    aaaa
+  aaaa    aaaa    aaaa    aaaa    aaaa
  b    .  b    .  .    c  b    c  b    c
  b    .  b    .  .    c  b    c  b    c
- dddd    dddd    ....    dddd    dddd
+  dddd    dddd    ....    dddd    dddd
  .    f  e    f  .    f  e    f  .    f
  .    f  e    f  .    f  e    f  .    f
- gggg    gggg    ....    gggg    gggg
+  gggg    gggg    ....    gggg    gggg
  So, to render a 1, only segments c and f would be turned on; the rest would be off. To render a 7, only segments a, c, and f would be turned on.
  
  The problem is that the signals which control the segments have been mixed up on each display. The submarine is still trying to display numbers by producing output on signal wires a through g, but those wires are connected to segments randomly. Worse, the wire/segment connections are mixed up separately for each four-digit display! (All of the digits within a display use the same connections, though.)
@@ -78,13 +78,13 @@ import Foundation
  cdfeb fcadb cdfeb cdbaf
  After some careful analysis, the mapping between signal wires and segments only make sense in the following configuration:
  
- dddd
+  dddd
  e    a
  e    a
- ffff
+  ffff
  g    b
  g    b
- cccc
+  cccc
  So, the unique signal patterns would correspond to the following digits:
  
  acedgfb: 8

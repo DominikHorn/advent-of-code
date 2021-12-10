c52ac205b5e9ffdf1272c8e3b1ac71ee64f5b4eb
Dominik Horn <dominik.horn@tum.de>
2021-12-10 15:33:58 +0100

    [10] solve second part

--- 2021/Sources/10/main.swift
+++ 2021/Sources/10/main.swift
@@ -53,43 +53,88 @@ import DequeModule
  In the above example, an illegal ) was found twice (2*3 = 6 points), an illegal ] was found once (57 points), an illegal } was found once (1197 points), and an illegal > was found once (25137 points). So, the total syntax error score for this file is 6+57+1197+25137 = 26397 points!
  
  Find the first illegal character in each corrupted line of the navigation subsystem. What is the total syntax error score for those errors?
+ 
+ --- Part Two ---
+
+ Now, discard the corrupted lines. The remaining lines are incomplete.
+
+ Incomplete lines don't have any incorrect characters - instead, they're missing some closing characters at the end of the line. To repair the navigation subsystem, you just need to figure out the sequence of closing characters that complete all open chunks in the line.
+
+ You can only use closing characters (), ], }, or >), and you must add them in the correct order so that only legal pairs are formed and all chunks end up closed.
+
+ In the example above, there are five incomplete lines:
+
+ [({(<(())[]>[[{[]{<()<>> - Complete by adding }}]])})].
+ [(()[<>])]({[<{<<[]>>( - Complete by adding )}>]}).
+ (((({<>}<{<{<>}{[]{[]{} - Complete by adding }}>}>)))).
+ {<[[]]>}<{[{[{[]{()[[[] - Complete by adding ]]}}]}]}>.
+ <{([{{}}[<[[[<>{}]]]>[]] - Complete by adding ])}>.
+ Did you know that autocomplete tools also have contests? It's true! The score is determined by considering the completion string character-by-character. Start with a total score of 0. Then, for each character, multiply the total score by 5 and then increase the total score by the point value given for the character in the following table:
+
+ ): 1 point.
+ ]: 2 points.
+ }: 3 points.
+ >: 4 points.
+ So, the last completion string above - ])}> - would be scored as follows:
+
+ Start with a total score of 0.
+ Multiply the total score by 5 to get 0, then add the value of ] (2) to get a new total score of 2.
+ Multiply the total score by 5 to get 10, then add the value of ) (1) to get a new total score of 11.
+ Multiply the total score by 5 to get 55, then add the value of } (3) to get a new total score of 58.
+ Multiply the total score by 5 to get 290, then add the value of > (4) to get a new total score of 294.
+ The five lines' completion strings have total scores as follows:
+
+ }}]])})] - 288957 total points.
+ )}>]}) - 5566 total points.
+ }}>}>)))) - 1480781 total points.
+ ]]}}]}]}> - 995444 total points.
+ ])}> - 294 total points.
+ Autocomplete tools are an odd bunch: the winner is found by sorting all of the scores and then taking the middle score. (There will always be an odd number of scores to consider.) In this example, the middle score is 288957 because there are the same number of scores smaller and larger than it.
+
+ Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score?
  */
 
 class Chunk {
   var start: OpenSymbol
-  var end: CloseSymbol?
+  
+  var hadEnd: Bool = false
+  var end: CloseSymbol {
+    start.matching
+  }
   
   var subchunks = [Chunk]()
   
   /// parses and returns the first chunk and its subchunks in the given string.
   /// Afterwards `description` contains the remaining substring
   private init(characters: inout Deque<Character>, globalIndex: inout Int) throws {
-    guard characters.count > 1 else {
-      throw ParsingError.incompleteLine
+    guard !characters.isEmpty else {
+      throw ParsingError.emptyChunk
     }
+    
     let first = characters.removeFirst()
     guard let chunkOpen = OpenSymbol(rawValue: first) else {
       throw ParsingError.illegalStart(first)
     }
     
-    
     self.start = chunkOpen
     globalIndex += 1
-    
     while !characters.isEmpty {
       guard let char = characters.first else {
         throw ParsingError.unexpectedEndOfSequence
       }
       
       if OpenSymbol(rawValue: char) != nil {
-        let subchunk = try Chunk(characters: &characters, globalIndex: &globalIndex)
-        subchunks.append(subchunk)
+        do {
+          let subchunk = try Chunk(characters: &characters, globalIndex: &globalIndex)
+          subchunks.append(subchunk)
+        }
+        
       } else if let close = CloseSymbol(rawValue: char) {
         globalIndex += 1
         characters.removeFirst()
         
         if close.matches(chunkOpen) {
-          self.end = close
+          self.hadEnd = true
           break // done with this chunk
         } else {
           throw ParsingError.corrupted(pos: globalIndex, expected: chunkOpen.matching, got: close)
@@ -112,8 +157,16 @@ class Chunk {
     return chunks
   }
   
+  var fixedEnds: [CloseSymbol] {
+    subchunks.flatMap { $0.fixedEnds } + (hadEnd ? [] : [end])
+  }
+  
+  var fixScore: Int {
+    fixedEnds.reduce(0) { 5 * $0 + $1.fixScore }
+  }
+  
   enum ParsingError: Error {
-    case incompleteLine
+    case emptyChunk
     case unexpectedEndOfSequence
     case illegalStart(_ raw: Character)
     case corrupted(pos: Int, expected: CloseSymbol, got: CloseSymbol)
@@ -145,6 +198,15 @@ class Chunk {
     func matches(_ symbol: OpenSymbol) -> Bool {
       self == symbol.matching
     }
+    
+    var fixScore: Int {
+      switch self {
+      case .round: return 1
+      case .square: return 2
+      case .squiggly: return 3
+      case .angle: return 4
+      }
+    }
   }
 }
 
@@ -158,45 +220,56 @@ extension Chunk.CloseSymbol: CustomStringConvertible {
 
 extension Chunk: CustomStringConvertible {
   var description: String {
-    let res = "\(start)\(subchunks.map { "\($0)" }.joined())"
-    if let end = end {
-      return res + end.description
-    }
-    
-    return res + "!"
+    "\(start)\(subchunks.map { "\($0)" }.joined())\(end)"
   }
 }
 
 struct NavigationSubsystem {
   typealias Line = [Chunk]
   
-  var validLines: [Line]
-  var corruptionScore: Int
+  let validLines: [Line]
+  let corruptionScore: Int
+  let fixScore: Int
   
   init(input: String) throws {
     let res = try input
       .split(separator: "\n")
-      .map { rawLine -> (validLine: Line?, corruptionScore: Int) in
+      .map { rawLine -> (validLine: Line?, corruptionScore: Int, corruptionReason: String?) in
         do {
-          return (try Chunk.from(description: String(rawLine)), 0)
+          let chunk = try Chunk.from(description: String(rawLine))
+          return (chunk, 0, nil)
         } catch Chunk.ParsingError.corrupted(pos: let pos, expected: let expected, got: let got) {
-          print("Expected `\(expected)`, but found `\(got)` instead at column \(pos)")
+          let reason = "Expected `\(expected)`, but found `\(got)` instead at column \(pos)"
           switch got {
-            case .round: return (nil, 3)
-            case .square: return (nil, 57)
-            case .squiggly: return (nil, 1197)
-            case .angle: return (nil, 25137)
+            case .round: return (nil, 3, reason)
+            case .square: return (nil, 57, reason)
+            case .squiggly: return (nil, 1197, reason)
+            case .angle: return (nil, 25137, reason)
           }
-        } catch Chunk.ParsingError.incompleteLine {
-          return (nil, 0)
         }
       }
     
     self.validLines = res.compactMap { $0.validLine }
     self.corruptionScore = res.reduce(0) { $0 + $1.corruptionScore }
+    self.fixScore = validLines
+      .map { $0.reduce(0) { $0 + $1.fixScore } }
+      .median()
   }
 }
 
+
+extension Array where Element: BinaryInteger {
+  // TODO: implement using quickselect
+  /// O(n log n)
+  func median() -> Element {
+    let l = sorted()
+    if l.count & 0x1 == 1 {
+      return l[l.count / 2]
+    } else {
+      return l[l.count / 2] + l[l.count / 2 + 1]
+    }
+  }
+}
 
 let testInput = """
 [({(<(())[]>[[{[]{<()<>>
@@ -318,6 +391,8 @@ let input = """
 
 let testNavSystem = try NavigationSubsystem(input: testInput)
 print(testNavSystem.corruptionScore)
+print(testNavSystem.fixScore)
 
 let navSystem = try NavigationSubsystem(input: input)
 print(navSystem.corruptionScore)
+print(navSystem.fixScore)

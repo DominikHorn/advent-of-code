499b7c6f55582a4ff232973796bc1fe3768958ca
Dominik Horn <dominik.horn@tum.de>
2021-12-15 19:08:10 +0100

    [15] solve second part

--- 2021/Sources/15/main.swift
+++ 2021/Sources/15/main.swift
@@ -34,6 +34,128 @@ import Foundation
  The total risk of this path is 40 (the starting position is never entered, so its risk is not counted).
  
  What is the lowest total risk of any path from the top left to the bottom right?
+ 
+ --- Part Two ---
+
+ Now that you know how to find low-risk paths in the cave, you can try to find your way out.
+
+ The entire cave is actually five times larger in both dimensions than you thought; the area you originally scanned is just one tile in a 5x5 tile area that forms the full map. Your original map tile repeats to the right and downward; each time the tile repeats to the right or downward, all of its risk levels are 1 higher than the tile immediately up or left of it. However, risk levels above 9 wrap back around to 1. So, if your original map had some position with a risk level of 8, then that same position on each of the 25 total tiles would be as follows:
+
+ 8 9 1 2 3
+ 9 1 2 3 4
+ 1 2 3 4 5
+ 2 3 4 5 6
+ 3 4 5 6 7
+ Each single digit above corresponds to the example position with a value of 8 on the top-left tile. Because the full map is actually five times larger in both dimensions, that position appears a total of 25 times, once in each duplicated tile, with the values shown above.
+
+ Here is the full five-times-as-large version of the first example above, with the original map in the top left corner highlighted:
+
+ 11637517422274862853338597396444961841755517295286
+ 13813736722492484783351359589446246169155735727126
+ 21365113283247622439435873354154698446526571955763
+ 36949315694715142671582625378269373648937148475914
+ 74634171118574528222968563933317967414442817852555
+ 13191281372421239248353234135946434524615754563572
+ 13599124212461123532357223464346833457545794456865
+ 31254216394236532741534764385264587549637569865174
+ 12931385212314249632342535174345364628545647573965
+ 23119445813422155692453326671356443778246755488935
+ 22748628533385973964449618417555172952866628316397
+ 24924847833513595894462461691557357271266846838237
+ 32476224394358733541546984465265719557637682166874
+ 47151426715826253782693736489371484759148259586125
+ 85745282229685639333179674144428178525553928963666
+ 24212392483532341359464345246157545635726865674683
+ 24611235323572234643468334575457944568656815567976
+ 42365327415347643852645875496375698651748671976285
+ 23142496323425351743453646285456475739656758684176
+ 34221556924533266713564437782467554889357866599146
+ 33859739644496184175551729528666283163977739427418
+ 35135958944624616915573572712668468382377957949348
+ 43587335415469844652657195576376821668748793277985
+ 58262537826937364893714847591482595861259361697236
+ 96856393331796741444281785255539289636664139174777
+ 35323413594643452461575456357268656746837976785794
+ 35722346434683345754579445686568155679767926678187
+ 53476438526458754963756986517486719762859782187396
+ 34253517434536462854564757396567586841767869795287
+ 45332667135644377824675548893578665991468977611257
+ 44961841755517295286662831639777394274188841538529
+ 46246169155735727126684683823779579493488168151459
+ 54698446526571955763768216687487932779859814388196
+ 69373648937148475914825958612593616972361472718347
+ 17967414442817852555392896366641391747775241285888
+ 46434524615754563572686567468379767857948187896815
+ 46833457545794456865681556797679266781878137789298
+ 64587549637569865174867197628597821873961893298417
+ 45364628545647573965675868417678697952878971816398
+ 56443778246755488935786659914689776112579188722368
+ 55172952866628316397773942741888415385299952649631
+ 57357271266846838237795794934881681514599279262561
+ 65719557637682166874879327798598143881961925499217
+ 71484759148259586125936169723614727183472583829458
+ 28178525553928963666413917477752412858886352396999
+ 57545635726865674683797678579481878968159298917926
+ 57944568656815567976792667818781377892989248891319
+ 75698651748671976285978218739618932984172914319528
+ 56475739656758684176786979528789718163989182927419
+ 67554889357866599146897761125791887223681299833479
+ Equipped with the full map, you can now find a path from the top left corner to the bottom right corner with the lowest total risk:
+
+ 11637517422274862853338597396444961841755517295286
+ 13813736722492484783351359589446246169155735727126
+ 21365113283247622439435873354154698446526571955763
+ 36949315694715142671582625378269373648937148475914
+ 74634171118574528222968563933317967414442817852555
+ 13191281372421239248353234135946434524615754563572
+ 13599124212461123532357223464346833457545794456865
+ 31254216394236532741534764385264587549637569865174
+ 12931385212314249632342535174345364628545647573965
+ 23119445813422155692453326671356443778246755488935
+ 22748628533385973964449618417555172952866628316397
+ 24924847833513595894462461691557357271266846838237
+ 32476224394358733541546984465265719557637682166874
+ 47151426715826253782693736489371484759148259586125
+ 85745282229685639333179674144428178525553928963666
+ 24212392483532341359464345246157545635726865674683
+ 24611235323572234643468334575457944568656815567976
+ 42365327415347643852645875496375698651748671976285
+ 23142496323425351743453646285456475739656758684176
+ 34221556924533266713564437782467554889357866599146
+ 33859739644496184175551729528666283163977739427418
+ 35135958944624616915573572712668468382377957949348
+ 43587335415469844652657195576376821668748793277985
+ 58262537826937364893714847591482595861259361697236
+ 96856393331796741444281785255539289636664139174777
+ 35323413594643452461575456357268656746837976785794
+ 35722346434683345754579445686568155679767926678187
+ 53476438526458754963756986517486719762859782187396
+ 34253517434536462854564757396567586841767869795287
+ 45332667135644377824675548893578665991468977611257
+ 44961841755517295286662831639777394274188841538529
+ 46246169155735727126684683823779579493488168151459
+ 54698446526571955763768216687487932779859814388196
+ 69373648937148475914825958612593616972361472718347
+ 17967414442817852555392896366641391747775241285888
+ 46434524615754563572686567468379767857948187896815
+ 46833457545794456865681556797679266781878137789298
+ 64587549637569865174867197628597821873961893298417
+ 45364628545647573965675868417678697952878971816398
+ 56443778246755488935786659914689776112579188722368
+ 55172952866628316397773942741888415385299952649631
+ 57357271266846838237795794934881681514599279262561
+ 65719557637682166874879327798598143881961925499217
+ 71484759148259586125936169723614727183472583829458
+ 28178525553928963666413917477752412858886352396999
+ 57545635726865674683797678579481878968159298917926
+ 57944568656815567976792667818781377892989248891319
+ 75698651748671976285978218739618932984172914319528
+ 56475739656758684176786979528789718163989182927419
+ 67554889357866599146897761125791887223681299833479
+ The total risk of this path is 315 (the starting position is still never entered, so its risk is not counted).
+
+ Using the full map, what is the lowest total risk of any path from the top left to the bottom right?
+ 
  */
 
 /// A\* graph node
@@ -206,8 +328,11 @@ extension Coordinate: CustomStringConvertible {
 
 struct Map {
   var riskLevels: [[Int]]
+  var tilingFactor: Int
   
-  init(description: String) throws {
+  init(description: String, tilingFactor: Int = 1) throws {
+    precondition(tilingFactor > 0)
+    
     let rows = description.split(separator: "\n")
     guard !rows.isEmpty, rows.allSatisfy({ $0.count == rows[0].count }) else {
       throw ParsingError.illegalInput
@@ -221,6 +346,7 @@ struct Map {
         return num
       }
     }
+    self.tilingFactor = tilingFactor
   }
   
   var start: Coordinate {
@@ -228,13 +354,31 @@ struct Map {
   }
   
   var end: Coordinate {
-    .init(x: riskLevels[riskLevels.count-1].count - 1, y: riskLevels.count - 1)
+    .init(x: tilingFactor * riskLevels[riskLevels.count-1].count - 1, y: tilingFactor * riskLevels.count - 1)
+  }
+  
+  func isInBounds(_ coord: Coordinate) -> Bool {
+    return coord.x >= 0 && coord.y >= 0
+      && coord.y < tilingFactor * riskLevels.count
+      && coord.x < tilingFactor * riskLevels[0].count
+  }
+  
+  func riskLevel(at coord: Coordinate) -> Int {
+    let ty = coord.y / riskLevels.count
+    let tx = coord.x / riskLevels[0].count
+    precondition(tx < tilingFactor && ty < tilingFactor)
+    
+    let y = coord.y % riskLevels.count
+    let x = coord.x % riskLevels[0].count
+    
+    var level = riskLevels[y][x] + ty + tx
+    while level > 9 { level -= 9 }
+    
+    return level
   }
   
   func path(from: Coordinate, to: Coordinate) -> [Coordinate] {
-    guard from.y >= 0, to.y >= 0, from.y < riskLevels.count, to.y < riskLevels.count,
-          from.x >= 0, to.x >= 0, from.x < riskLevels[from.y].count, to.x < riskLevels[to.y].count
-    else { return [] }
+    guard isInBounds(from), isInBounds(to) else { return [] }
     
     let heuristic = { (a: Coordinate, b: Coordinate) -> Int in
       abs(a.x - b.x) + abs(a.y - b.y)
@@ -255,15 +399,10 @@ struct Map {
       
       // expand current
       current.neighbors.forEach { neighbor in
-        guard neighbor.y >= 0,
-              neighbor.y < riskLevels.count,
-              neighbor.x >= 0,
-              neighbor.x < riskLevels[neighbor.y].count,
-              !closedList.contains(neighbor)
-        else { return }
+        guard isInBounds(neighbor), !closedList.contains(neighbor) else { return }
         
         // cost for going to neighbor from this node
-        let newCost = current.cost + riskLevels[neighbor.y][neighbor.x]
+        let newCost = current.cost + riskLevel(at: neighbor)
         
         // if we already know of a way to reach neighbor that is cheaper, discard
         if let oldCost = openList.cost(at: neighbor), oldCost < newCost { return }
@@ -278,13 +417,13 @@ struct Map {
   }
   
   func cost(path: [Coordinate]) -> Int {
-    path.dropFirst().reduce(0) { $0 + riskLevels[$1.y][$1.x] }
+    path.dropFirst().reduce(0) { $0 + riskLevel(at: $1) }
   }
   
   func description(highlighting: Set<Coordinate>) -> String {
-    riskLevels.indices.map { y in
-      riskLevels[y].indices.map { x in
-        highlighting.contains(.init(x: x, y: y)) ? "." : "\(riskLevels[y][x])"
+    (0..<(riskLevels.count*tilingFactor)).map { y in
+      (0..<(riskLevels[y % riskLevels.count].count*tilingFactor)).map { x in
+        highlighting.contains(.init(x: x, y: y)) ? "." : "\(riskLevel(at: .init(x: x, y: y)))"
       }.joined()
     }.joined(separator: "\n")
   }
@@ -407,12 +546,31 @@ let input = """
 9989369421519694978991559748449571799694789957167495979999319838284795345682191854697182819189591892
 """
 
-let testMap = try Map(description: testInput)
-let testPath = testMap.path(from: testMap.start, to: testMap.end)
-//print(testMap.description(highlighting: .init(testPath)))
-print(testMap.cost(path: testPath))
+func solve1() throws {
+  print("part 1:")
+  let testMap = try Map(description: testInput)
+  let testPath = testMap.path(from: testMap.start, to: testMap.end)
+  print(testMap.description(highlighting: .init(testPath)))
+  print(testMap.cost(path: testPath))
 
-let map = try Map(description: input)
-let path = map.path(from: map.start, to: map.end)
-//print(map.description(highlighting: .init(path)))
-print(map.cost(path: path))
+  let map = try Map(description: input)
+  let path = map.path(from: map.start, to: map.end)
+  //print(map.description(highlighting: .init(path)))
+  print(map.cost(path: path))
+}
+
+func solve2() throws {
+  print("part 2:")
+  let testMap = try Map(description: testInput, tilingFactor: 5)
+  let testPath = testMap.path(from: testMap.start, to: testMap.end)
+  print(testMap.description(highlighting: .init(testPath)))
+  print(testMap.cost(path: testPath))
+
+  let map = try Map(description: input, tilingFactor: 5)
+  let path = map.path(from: map.start, to: map.end)
+  //print(map.description(highlighting: .init(path)))
+  print(map.cost(path: path))
+}
+
+try solve1()
+try solve2()

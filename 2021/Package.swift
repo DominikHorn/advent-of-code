ea6a5e648a6901b71c3ced06f8e55c223eb7682f
Dominik Horn <dominik.horn@tum.de>
2021-12-17 23:33:19 +0100

    [18] add barebones executable target

--- 2021/Package.swift
+++ 2021/Package.swift
@@ -28,6 +28,7 @@ let package = Package(
         .executableTarget(name: "14"),
         .executableTarget(name: "15"),
         .executableTarget(name: "16"),
-        .executableTarget(name: "17")
+        .executableTarget(name: "17"),
+        .executableTarget(name: "18")
     ]
 )
